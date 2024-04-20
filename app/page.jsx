774e58ed15dc88543a3c972a5921d1e80c26f616
TroyMoses <mugabimoses07@gmail.com>
2024-04-20 23:55:36 +0600

    login

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,12 @@
-export default function HomePage() {
+import styles from "./ui/login/login.module.css";
+import LoginForm from "./ui/login/loginForm/loginForm";
 
-    return (
-        <div>
-            HomePage
-        </div>
-    );
-}
+const LoginPage = () => {
+  return (
+    <div className={styles.container}>
+      <LoginForm/>
+    </div>
+  );
+};
+
+export default LoginPage;
